6eb83535008dc99cf2a16945e668698019215c73
ItalusPrado <italusprado@acad.ifce.edu.br>
2017-09-01 11:47:22 -0300

    Subindo projeto com alguns erros na funcao sucessora

--- PathNSquares/ViewController.swift
+++ PathNSquares/ViewController.swift
@@ -9,17 +9,135 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    let squaresQtd = 10
+    let ambientSize = 6
+    
+    var ambient = [[Int]]()
+    var positionsX = [Int]()
+    var positionsY = [Int]()
+    
+    var vertexPositions = [[Int]]()
+    var sucessors = [[[Int]]]()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        createAmbient()
+        for i in 0..<ambientSize{
+            print(ambient[i])
+        }
+        for i in 0..<vertexPositions.count{
+            var vector = [[Int]]()
+            for j in i..<vertexPositions.count{
+                if i != j{
+                    if !createSucessors(firstVertex: vertexPositions[i], secondVertex: vertexPositions[j]){
+                         print("\(vertexPositions[i]) - \(vertexPositions[j])")
+                        vector.append(vertexPositions[j])
+                    }
+                }
+            }
+            sucessors.append(vector)
+        }
+//        createSucessors(firstVertex: vertexPositions[0], secondVertex: vertexPositions[7])
     }
-
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
-
+    
+    
+    // Preparando os quadrados no vetor
+    func createAmbient(){
+        var array = [Int]()
+        for _ in 0..<ambientSize{
+            array.append(0)
+        }
+        for _ in 0..<ambientSize{
+            ambient.append(array)
+        }
+        for _ in 0..<squaresQtd{
+            let x = Int(2+arc4random()%UInt32(ambientSize-5))
+            let y = Int(1+arc4random()%UInt32(ambientSize-3))
+            
+            positionsX.append(x)
+            positionsY.append(y)
+            self.ambient[y][x] = 1
+            self.ambient[y+1][x] = 1
+            self.ambient[y][x+1] = 1
+            self.ambient[y+1][x+1] = 1
+            vertex(point: [y,x])
+            //print("\(x) - \(y)")
+        }
+    }
+    
+    // Definindo as vertices de cada quadrado
+    func vertex(point: [Int]){
+        if ambient[point[0]-1][point[1]-1] != 1 && ambient[point[0]-1][point[1]-1] != 2{
+            ambient[point[0]-1][point[1]-1] = 2
+            vertexPositions.append([point[1]-1,point[0]-1])
+        }
+        if ambient[point[0]+2][point[1]-1] != 1 && ambient[point[0]+2][point[1]-1] != 2{
+            ambient[point[0]+2][point[1]-1] = 2
+            vertexPositions.append([point[1]-1,point[0]+2])
+        }
+        if ambient[point[0]-1][point[1]+2] != 1 && ambient[point[0]-1][point[1]+2] != 2{
+            ambient[point[0]-1][point[1]+2] = 2
+            vertexPositions.append([point[1]+2,point[0]-1])
+        }
+        if ambient[point[0]+2][point[1]+2] != 1 && ambient[point[0]+2][point[1]+2] != 2{
+            ambient[point[0]+2][point[1]+2] = 2
+            vertexPositions.append([point[1]+2,point[0]+2])
+        }
+    }
+    
+    func createSucessors(firstVertex: [Int], secondVertex: [Int]) -> Bool{
+        let equation = generateEquation(firstPoint: firstVertex, secondPoint: secondVertex)
+        var cut = false
+        if firstVertex[0] < secondVertex[0]{
+            for i in firstVertex[0]...secondVertex[0]{
+                if (-(equation[0]*i)+equation[2])/equation[1] >= 0 && (-(equation[0]*i)+equation[2])/equation[1] < ambientSize{
+                    if ambient[(-(equation[0]*i)+equation[2])/equation[1]][i] == 1{
+                        cut = true
+                    }
+                }
+            }
+        } else if secondVertex[0] == secondVertex[0]{
+            if firstVertex[1] < secondVertex[1]{
+                for i in firstVertex[1]...secondVertex[1]{
+                    if ambient[firstVertex[0]][i] == 1{
+                        cut = true
+                    }
+                }
+            } else {
+                for i in secondVertex[1]...firstVertex[1]{
+                    if ambient[firstVertex[0]][i] == 1{
+                        cut = true
+                    }
+                }
+            }
+            
+        } else {
+            for i in secondVertex[0]...firstVertex[0]{
+                if (-(equation[0]*i)+equation[2])/equation[1] >= 0 && (-(equation[0]*i)+equation[2])/equation[1] < ambientSize{
+                    if ambient[(-(equation[0]*i)+equation[2])/equation[1]][i] == 1{
+                        cut = true
+                    }
+                }
+            }
+        }
+        return cut
+    }
+    // Criando função da reta [1] = Y ; [0] = X
+    func generateEquation(firstPoint: [Int], secondPoint: [Int]) -> [Int]{
+        let a = -(secondPoint[1]-firstPoint[1])
+        let b = secondPoint[0]-firstPoint[0]
+        let c = a*(-firstPoint[0])+b*(-firstPoint[1])
+        //print("\(a)x+\(b)y+\(c)")
+        return [a,b,c]
+        
+    }
+    
+    
 }
 
