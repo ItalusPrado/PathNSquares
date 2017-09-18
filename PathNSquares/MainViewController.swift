2f61b975d25f764c4552bccbcfddc14d77b9fbd8
ItalusPrado <italusprado@acad.ifce.edu.br>
2017-09-18 08:32:58 -0300

    girando a camera da main

--- PathNSquares/MainViewController.swift
+++ PathNSquares/MainViewController.swift
@@ -12,6 +12,8 @@ import SceneKit
 class MainViewController: UIViewController {
 
     @IBOutlet weak var scnView: SCNView!
+    @IBOutlet weak var randomBtn: UIButton!
+    @IBOutlet weak var chooseBtn: UIButton!
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -25,17 +27,33 @@ class MainViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
     
+    override func viewDidLayoutSubviews() {
+        self.randomBtn.layer.cornerRadius = self.randomBtn.frame.size.height/4
+        self.chooseBtn.layer.cornerRadius = self.randomBtn.layer.cornerRadius
+        
+        self.randomBtn.clipsToBounds = true
+        self.chooseBtn.clipsToBounds = true
+    }
+    
     func createScene(){
         let scene = SCNScene()
         self.scnView.scene = scene
-        self.scnView.showsStatistics = true
-        self.scnView.allowsCameraControl = true
+        
+        // Center point
+        let centerNode = SCNNode(geometry: SCNBox(width: 0.01, height: 0.01, length: 0.01, chamferRadius: 0))
+        centerNode.position = SCNVector3(0,0,0)
+        centerNode.physicsBody = SCNPhysicsBody.dynamic()
+        centerNode.physicsBody?.isAffectedByGravity = false
+        centerNode.physicsBody?.angularDamping = 0
+        centerNode.physicsBody?.angularVelocity = SCNVector4(1,1,1,0.2)
+        scene.rootNode.addChildNode(centerNode)
         
         // Camera
         let cameraNode = SCNNode()
         cameraNode.camera = SCNCamera()
         cameraNode.position = SCNVector3(0,0,10)
-        scene.rootNode.addChildNode(cameraNode)
+        centerNode.addChildNode(cameraNode)
+        scnView.pointOfView = cameraNode
         
         // create and add a light to the scene
         let lightNode = SCNNode()
@@ -58,9 +76,8 @@ class MainViewController: UIViewController {
     func squares(){
         
         for i in -4...4{
-            for j in -2 ... 2{
-                let position = SCNVector3(i*2,j*2,4-Int(arc4random()%10))
-                
+            for j in -4 ... 4{
+                let position = SCNVector3(i*2,j*2,5-Int(arc4random()%10))
                 
                 let box = SCNBox(width: 1, height: 1, length: 1, chamferRadius: 0)
                 box.materials.first?.diffuse.contents = UIColor.red
